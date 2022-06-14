1c9fd92093a039416c5110e0b1f505d5805917b3
ChaitanyaChandra <chay@outlook.in>
2022-06-14 23:33:01 +0530

    automated commit #487

--- code.sh
+++ code.sh
@@ -8,3 +8,18 @@ cd spec/
 npm  install
 node index.js > node.logs 2>&1 &
 ps -ef | grep "index.js" > run.log
+
+sudo yum install wget -y 
+
+# download rpm using wget
+wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
+
+# install downloaded rpm
+sudo rpm -U ./amazon-cloudwatch-agent.rpm
+
+# create a dir to avoid errors
+sudo mkdir -p /usr/share/collectd/
+sudo touch /usr/share/collectd/types.db
+
+# run agent 
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:config.json
